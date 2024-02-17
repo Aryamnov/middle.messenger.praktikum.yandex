e85d0ae9a009b76bcf4b9cee9d8e686754c0bf89
Andrey Aryamnov <aryamnov@gmail.com>
2024-02-17 13:05:40 +0700

    spritn_1: [+] Настроить сборку проекта

--- src/pages/server-error/index.js
+++
@@ -1,9 +0,0 @@
-import { compile } from 'handlebars';
-import './server-error.scss';
-import template from './index.hbs?raw';
-
-const app = document.getElementById('app');
-
-const navigate = compile(template);
-
-app.innerHTML = navigate({});
--- src/pages/signin/index.js
+++
@@ -1,9 +0,0 @@
-import { compile } from 'handlebars';
-import './signin.scss';
-import template from './index.hbs?raw';
-
-const app = document.getElementById('app');
-
-const navigate = compile(template);
-
-app.innerHTML = navigate({});
