afdc6d259b7aba4b85700d45455747e7e02375fa
Andrey Aryamnov <aryamnov@gmail.com>
2024-02-17 14:29:12 +0700

    spritn_1: [+] Добавить сервер на express

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,19 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
   },
-  plugins: [handlebars()],
+  plugins: [
+      handlebars(),
+      copy({
+        targets: [
+          { src: './src/server/server.js', dest: './dist' }
+        ]
+      })
+  ],
 });
